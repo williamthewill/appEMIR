7c6baa6c586b43619778f8ae0ae323f27340e1c8
william muller <william-code@hotmail.com>
2018-05-08 11:46:06 -0300

    calculando aliquota e soma de debitos

--- public/js/model/calculadora-financeira.js
+++ public/js/model/calculadora-financeira.js
@@ -1,11 +1,25 @@
 /*eslint no-unused-vars: */
+/* global ContasController */
 
-class CalculadoraFinanceira {
-	constructor (constas){
-		this.contas = constas;
-	}
-	
-	aliquota(){}
-	impostoRenda(){}
-	somaDeDebitos(){}
+function calcularAliquota(){
+	const porcentagemContribuicao = 0.06;
+	let receita = parseFloat(document.querySelector('.aliquota').value);
+
+	let aliquota = receita*porcentagemContribuicao;
+
+	document.querySelector('.valor-aliquota').innerText = aliquota;
+}
+
+function impostoRenda(){}
+
+function somaDeDebitos() {
+	let contasController = new ContasController();
+
+	contasController.listarContas().then(listaContas => {
+		let debitoTotal = 0;
+		for(let i = 0; i < listaContas.length; i++) {
+			debitoTotal += parseFloat(listaContas[i].valor);
+		}
+		document.querySelector('.total-contas').innerText = `R$ ${debitoTotal} reais`;
+	});
 }
